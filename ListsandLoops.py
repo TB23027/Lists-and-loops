5053c40e78368b0b3c11029b6f6828d7acb15b80
Torr Beaman <tb23027@my.westlake.school.nz>
2025-02-12 10:45:22 +1300

    completed task 1,2,3,4 and doing 5

--- ListsandLoops.py
+++ ListsandLoops.py
@@ -1,3 +1,22 @@
 for number in range(1, 11):
     print(number)
 
+numbers = ["1", "2", "3", "4", "5"]
+for number in numbers:
+    print(number)
+
+multiply =  int(input("Enter a number: "))
+for i in range(1,11):
+    print(f"{multiply} x {i} = {multiply * i}")
+
+for number in range(1, 21):
+    if number % 2 == 0:
+         print(number)
+
+import math
+base = [1, 2, 3, 4, 5]
+for num in numbers:
+    sqrt = math.sqrt(base)
+    print(f"The square root of {base} is {sqrt}")
+
+           
