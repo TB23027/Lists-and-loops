ef90cbb28734469410123d139697c3298589110c
Torr Beaman <tb23027@my.westlake.school.nz>
2025-02-13 14:48:28 +1300

    Finished up to number 10

--- ListsandLoops.py
+++ ListsandLoops.py
@@ -1,8 +1,8 @@
 for number in range(1, 11):
     print(number)
 
-numbers = ["1", "2", "3", "4", "5"]
-for number in numbers:
+list_1 = ["1", "2", "3", "4", "5"]
+for number in list_1:
     print(number)
 
 multiply =  int(input("Enter a number: "))
@@ -13,10 +13,34 @@ for number in range(1, 21):
     if number % 2 == 0:
          print(number)
 
-import math
-base = [1, 2, 3, 4, 5]
-for num in numbers:
-    sqrt = math.sqrt(base)
-    print(f"The square root of {base} is {sqrt}")
 
-           
+squarenum = [1,2,3,4,5]
+for number in squarenum: 
+    print(int(number) * (number))
+
+list_2 = [1,2,3,4,5,6,7,8,9,10]
+
+list_2.reverse()
+for reversed in list_2:
+    print(reversed)
+
+list_3 = ["Alice", "Bob", "Charlie"]
+for name in list_3:
+    print(f"Hello, {name}!")
+
+total_sum = 0
+for number in range(1,11):
+    total_sum += number
+    print("The sum of the first 10 Natural numbers is:", total_sum)
+
+list_4 = [10,20,30,40,50]
+for number in list_4:
+    if number >= 25:
+        print(number)
+
+sentence = input("please Enter a sentence: ")
+words = sentence.split()
+for word in words:
+    print(word)
+
+
